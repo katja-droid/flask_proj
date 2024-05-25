76067ddb43c774e4e2e749db654b3642a3258e42
katja-droid <sosnyukk@gmail.com>
2024-05-25 08:28:39 +0300

    fc

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+# Sample data for products
+products = [
+    {"id": 1, "name": "Product 1", "price": 10},
+    {"id": 2, "name": "Product 2", "price": 20},
+    {"id": 3, "name": "Product 3", "price": 30},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
