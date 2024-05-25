1f6802d4a3c878b3e206be9b0f3cd11b9c092c7c
katja-droid <sosnyukk@gmail.com>
2024-05-25 08:48:14 +0300

    nn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -17,4 +18,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
